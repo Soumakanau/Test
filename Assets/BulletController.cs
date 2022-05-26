22e0da04f0ece6d38908ebf6db750a36aae2c559
kanau_19 <kananaru0808@gmail.com>
2022-05-26 13:32:29 +0900

    初めてのコッミト

---
+++ Assets/BulletController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController: MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject explosionPrefab;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0.2f, 0);
+
+        if (transform.position.y > 5)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+        void OnTriggerEnter2D (Collider2D coll)
+        {
+        GameObject.Find("Canvas").GetComponent<UIController>().AddScore();
+
+            GameObject effect=Instantiate(explosionPrefab, transform.position, Quaternion.identity) as GameObject;
+             Destroy(effect, 1.0f);
+            
+            Destroy(coll.gameObject); 
+            Destroy(gameObject);
+        }
+    }
+
